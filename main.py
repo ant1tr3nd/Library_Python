baf530cc966316f950637728986e237678805980
Alexander Ant1tr3nd <ant1tr3nd@yandex.ru>
2024-11-10 02:23:38 +0300

    Первая версия библиотеки (ChatGPT одобрил)

--- main.py
+++ main.py
@@ -1,52 +1,80 @@
-class Book:
-    def __init__(self, book_name: str, release_date: str, count_pages: int, book_in_library: bool = True):
-        try:
-            if type(book_name) == str and type(release_date) == str and type(count_pages) == int and type(
-                    book_in_library) == bool:
-                self.__book_name = book_name
-                self.__release_date = release_date
-                self.__count_pages = count_pages
-                self.__book_in_library = book_in_library
-            else:
-                raise TypeError('Были введены данные неверного типа (str, str, int, bool(Не обязательно))')
-        except TypeError as Error:
-            print(Error)
-    def __str__(self):
-        print(f'Книга: {self.__book_name}\nДата выпуска: {self.__release_date}\n В ней {self.__count_pages} страниц(ы)')
-    def get_book_name(self):
-        print(self.__book_name)
-    def get_book_release_data(self):
-        print(self.__release_date)
-    def get_book_count_pages(self):
-        print(self.__release_date)
-
-
-
-class Library:
-    __Books_in_library = 0
-    __Books_list = list()
-    __clients_base = dict()
-
-    def add_new_book(self, book: Book):
-        self.__Books_in_library += 1
-        self.__Books_list.append(book)
-
-    def give_book(self, book: Book, client: Client):
-        self.__Books_in_library -= 1
-        self.__Books_in_library.remove(book)
-        if hash(client) in self.__clients_base.keys():
-            self.__clients_base[hash(client)]['Books_in_usage'].append(book)
-        else:
-            self.__clients_base[hash(client)] = {'Name': client.name,
-                                                 'Phone': client.phone,
-                                                 'Age': client.age,
-                                                 'Books_in_usage': [book]}
-
 class People:
-    pass
-
-class Client(People):
     def __init__(self, name, age, phone):
         self.name = name
         self.age = age
         self.phone = phone
+
+class Client(People):
+    def __init__(self, name, age, phone):
+        super().__init__(name, age, phone)
+        self.book_list = list()
+
+
+class Book:
+    def __init__(self, book_name: str, release_date: str, count_pages: int, book_in_library: bool = True):
+        try:
+            if (isinstance(book_name, str) and isinstance(release_date, str) and isinstance(count_pages,
+                    int)) and isinstance(book_in_library, bool):
+                self._book_name = book_name
+                self._release_date = release_date
+                self._count_pages = count_pages
+                self.book_in_library = book_in_library
+            else:
+                raise TypeError('Были введены данные неверного типа (str, str, int, bool(Не обязательно))')
+        except TypeError as Error:
+            print(Error)
+
+    def __repr__(self):
+        return f'Книга: {self._book_name}'
+
+    def __str__(self):
+        return f'Книга: {self._book_name}\nДата выпуска: {self._release_date}\nВ ней {self._count_pages} страниц(ы)'
+
+    def get_book_name(self):
+        return self._book_name
+
+    def get_book_release_data(self):
+        return self._release_date
+
+    def get_book_count_pages(self):
+        return self._count_pages
+
+
+
+class Library:
+    _books_in_library = 0
+    _books_list = list()
+    _clients_base = dict()
+
+    def add_new_book(self, book: Book):
+        self._books_in_library += 1
+        self._books_list.append(book)
+
+    @staticmethod
+    def check_book_in_library(book):
+        return book.book_in_library
+
+    def give_book(self, book: Book, client: Client):
+        if self.check_book_in_library(book):
+            self._books_in_library -= 1
+            self._books_list.remove(book)
+            book.book_in_library = False
+            if hash(client) in self._clients_base.keys():
+                self._clients_base[hash(client)]['Books_in_usage'].append(book)
+            else:
+                self._clients_base[hash(client)] = {'Name': client.name,
+                                                     'Phone': client.phone,
+                                                     'Age': client.age,
+                                                     'Books_in_usage': [book]}
+            client.book_list.append(book)
+        else:
+            print('Книга сейчас не находиться в библиотеке')
+
+    def return_book(self, client, book):
+            if book in self._clients_base[hash(client)]['Books_in_usage']:
+                self._clients_base[hash(client)]['Books_in_usage'].remove(book)
+                self._books_in_library += 1
+                self._books_list.append(book)
+                book.book_in_library = True
+            else:
+                print('У клиента нет данной книги')
